0c6eb95d225e76ceed91241073e2b65c76122264
Dinārs Kemzāns 221RDB321 <125220545+DinaarsKemzaans@users.noreply.github.com>
2023-04-23 13:29:35 +0000

    adding corrext link

--- setup.py
+++ setup.py
@@ -3,11 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="timechecker",
     version="0.0.5",
-    author="Edidiong Etuk",
-    author_email="edeediong@gmail.com",
-    url="https://bit.ly/edeediong-resume",
     description="An aplication that informs you of the time in different locations and timezones",
-    packages="Dinārs Kemzāns 221RDB321_devops3MD",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
